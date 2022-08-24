ed81e6fa5e881a926e8e508294e41779874c4b3f
b166erobot <bernard0freitas@outlook.com>
2022-08-23 22:41:39 -0300

    removendo algumas tralhas

--- adicionar_arquivos_config_git.sh
+++ adicionar_arquivos_config_git.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # https://www.atlassian.com/git/tutorials/dotfiles
 
-alias config='git --git-dir=/home/none/.cfg/ --work-tree=/home/none'
+# todos os arquivos à serem adicionados não podem conter espaços entre o nome.
+
+# alias config='git --git-dir=/home/none/.cfg/ --work-tree=/home/none'
+
+function config {
+    git --git-dir=/home/none/.cfg/ --work-tree=/home/none $@
+}
 
 
 config add ~/.bash_aliases
@@ -9,76 +15,6 @@ config add ~/.bashrc
 config add ~/.config/Thunar/accels.scm
 config add ~/.config/Thunar/uca.xml
 config add ~/.config/i3/config
-config add ~/.config/xfce4/Verve/history
-config add ~/.config/xfce4/desktop/icons.screen.latest.rc
-config add ~/.config/xfce4/desktop/icons.screen0-1556x884.rc
-config add ~/.config/xfce4/desktop/icons.screen0-1584x807.rc
-config add ~/.config/xfce4/desktop/icons.screen0-1584x830.rc
-config add ~/.config/xfce4/desktop/icons.screen0-1584x856.rc
-config add ~/.config/xfce4/desktop/icons.screen0-1584x857.rc
-config add ~/.config/xfce4/helpers.rc
-config add ~/.config/xfce4/panel/launcher-1/15930947881.desktop
-config add ~/.config/xfce4/panel/launcher-1/15941558951.desktop
-config add ~/.config/xfce4/panel/launcher-17/15913191951.desktop
-config add ~/.config/xfce4/panel/launcher-17/15927534791.desktop
-config add ~/.config/xfce4/panel/launcher-17/15938220015.desktop
-config add ~/.config/xfce4/panel/launcher-18/15913191952.desktop
-config add ~/.config/xfce4/panel/launcher-18/15927536073.desktop
-config add ~/.config/xfce4/panel/launcher-18/15938219214.desktop
-config add ~/.config/xfce4/panel/launcher-19/15916563651.desktop
-config add ~/.config/xfce4/panel/launcher-19/15927536564.desktop
-config add ~/.config/xfce4/panel/launcher-19/15939575231.desktop
-config add ~/.config/xfce4/panel/launcher-20/15913191954.desktop
-config add ~/.config/xfce4/panel/launcher-20/15927537405.desktop
-config add ~/.config/xfce4/panel/launcher-20/15938219143.desktop
-config add ~/.config/xfce4/panel/launcher-23/15913206193.desktop
-config add ~/.config/xfce4/panel/launcher-23/15927538296.desktop
-config add ~/.config/xfce4/panel/launcher-23/15938218941.desktop
-config add ~/.config/xfce4/panel/launcher-24/15913206344.desktop
-config add ~/.config/xfce4/panel/launcher-24/15928333441.desktop
-config add ~/.config/xfce4/panel/launcher-24/15938220236.desktop
-config add ~/.config/xfce4/panel/launcher-26/15913206756.desktop
-config add ~/.config/xfce4/panel/launcher-26/15927607238.desktop
-config add ~/.config/xfce4/panel/launcher-26/15938219062.desktop
-config add ~/.config/xfce4/panel/launcher-27/15914599201.desktop
-config add ~/.config/xfce4/panel/launcher-27/15927607359.desktop
-config add ~/.config/xfce4/panel/launcher-27/159382232111.desktop
-config add ~/.config/xfce4/panel/launcher-28/159276076610.desktop
-config add ~/.config/xfce4/panel/launcher-29/159276191511.desktop
-config add ~/.config/xfce4/panel/launcher-29/159382216810.desktop
-config add ~/.config/xfce4/panel/launcher-30/15926097121.desktop
-config add ~/.config/xfce4/panel/launcher-30/15950930541.desktop
-config add ~/.config/xfce4/panel/launcher-7/15913205731.desktop
-config add ~/.config/xfce4/panel/launcher-9/15913206062.desktop
-config add ~/.config/xfce4/panel/whiskermenu-29.rc
-config add ~/.config/xfce4/panel/whiskermenu-9.rc
-config add ~/.config/xfce4/panel/xfce4-clipman-actions.xml
-config add ~/.config/xfce4/panel/xfce4-notes-plugin-7.rc
-config add ~/.config/xfce4/panel/xfce4-verve-plugin-1.rc
-config add ~/.config/xfce4/terminal/accels.scm
-config add ~/.config/xfce4/terminal/terminalrc
-config add ~/.config/xfce4/xfce4-notes.gtkrc
-config add ~/.config/xfce4/xfce4-notes.rc
-config add ~/.config/xfce4/xfce4-screenshooter
-config add ~/.config/xfce4/xfce4-taskmanager.rc
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/parole.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/ristretto.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-appfinder.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-screensaver.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-settings-editor.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-settings-manager.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-config add ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 config add ~/.dotfiles_cola
 config add ~/.gitconfig
 config add ~/.gitignore
@@ -94,4 +30,4 @@ config add ~/formatação/interfaces.py
 config add ~/formatação/post_install.py
 config add ~/formatação/pre_install.py
 config add ~/sites_interessantes.txt
-config add ~/git/dicas\ para\ comandos\ git.txt
+config add ~/git/dicas_para_comandos_git.txt
